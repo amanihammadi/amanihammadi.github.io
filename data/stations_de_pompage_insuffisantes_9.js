43d7c1e89b820e1a0331cd08ef3e3a864ae94f55
amanihammadi <74589567+amanihammadi@users.noreply.github.com>
2021-11-28 15:37:29 +0100

    Add files via upload

---
+++ data/stations_de_pompage_insuffisantes_9.js
@@ -0,0 +1,13 @@
+var json_stations_de_pompage_insuffisantes_9 = {
+"type": "FeatureCollection",
+"name": "stations_de_pompage_insuffisantes_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nom": "SR6", "ID": "1" }, "geometry": { "type": "Point", "coordinates": [ 10.10296285964973, 33.87381255782014 ] } },
+{ "type": "Feature", "properties": { "nom": "SP3", "ID": "2" }, "geometry": { "type": "Point", "coordinates": [ 10.12509188707927, 33.85463766762604 ] } },
+{ "type": "Feature", "properties": { "nom": "SR9", "ID": "3" }, "geometry": { "type": "Point", "coordinates": [ 10.11858497316602, 33.87787433344757 ] } },
+{ "type": "Feature", "properties": { "nom": "SR4", "ID": "4" }, "geometry": { "type": "Point", "coordinates": [ 10.11112628097482, 33.89302967056433 ] } },
+{ "type": "Feature", "properties": { "nom": "SP1", "ID": "5" }, "geometry": { "type": "Point", "coordinates": [ 10.10057700284952, 33.9093305458126 ] } },
+{ "type": "Feature", "properties": { "nom": "SP2", "ID": "6" }, "geometry": { "type": "Point", "coordinates": [ 10.09767600632864, 33.91099559887942 ] } }
+]
+}
